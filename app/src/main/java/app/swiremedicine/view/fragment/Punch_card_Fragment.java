a48f8a9a4e6f41f9efd03a6f720b3b173bb6517a
Administrator <2561170646@qq.com>
2017-10-24 20:25:58 +0800

    //郝建澄添加定位功能

--- app/src/main/java/app/swiremedicine/view/fragment/Punch_card_Fragment.java
+++ app/src/main/java/app/swiremedicine/view/fragment/Punch_card_Fragment.java
@@ -3,9 +3,16 @@ package app.swiremedicine.view.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 
 import app.swiremedicine.R;
 
@@ -14,10 +21,79 @@ import app.swiremedicine.R;
  */
 
 public class Punch_card_Fragment extends Fragment {
+    private TextView dingweitv;
+    public AMapLocationClient mLocationClient = null;
+    //声明定位回调监听器
+    public AMapLocationListener mLocationListener = new AMapLocationListener() {
+
+        @Override
+        public void onLocationChanged(AMapLocation amapLocation) {
+            // TODO Auto-generated method stub
+            if (amapLocation != null) {
+                if (amapLocation.getErrorCode() == 0) {
+                    //可在其中解析amapLocation获取相应内容。
+                    double locationType = amapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见定位类型表
+                    double latitude = amapLocation.getLatitude();//获取纬度
+                    Log.e("Amap==经度：纬度", "locationType:"+locationType+",latitude:"+latitude);
+                    dingweitv.setText("精度："+locationType+"--维度："+latitude);
+                }else {
+                    //定位失败时，可通过ErrCode（错误码）信息来确定失败的原因，errInfo是错误信息，详见错误码表。
+                    Log.e("AmapError","location Error, ErrCode:"
+                            + amapLocation.getErrorCode() + ", errInfo:"
+                            + amapLocation.getErrorInfo());
+                }
+            }
+        }
+    };
+
+    //声明AMapLocationClientOption对象
+    public AMapLocationClientOption mLocationOption = null;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.punch_card_fragment, container, false);
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        dingweitv=getActivity().findViewById(R.id.dingweitv);
+        mLocationClient = new AMapLocationClient(getActivity().getApplicationContext());
+        //设置定位回调监听
+        mLocationClient.setLocationListener(mLocationListener);
+
+        //初始化AMapLocationClientOption对象
+        mLocationOption = new AMapLocationClientOption();
+        //设置定位模式为AMapLocationMode.Battery_Saving，低功耗模式。
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Battery_Saving);
+
+
+        //给定位客户端对象设置定位参数
+        mLocationClient.setLocationOption(mLocationOption);
+        //启动定位
+        mLocationClient.startLocation();
+
+        /**
+         * 获取一次定位
+         */
+        //该方法默认为false，true表示只定位一次
+        mLocationOption.setOnceLocation(true);
+
+    }
+//    @Override
+//    protected void onDestroy() {
+//        // TODO Auto-generated method stub
+//        super.onDestroy();
+//
+//        mLocationClient.stopLocation();
+//    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mLocationClient.stopLocation();
     }
 }
